06bf51ec43b1f8992c4634e11b050cbb22c206b0
supermt_VM <supertempler@gmail.com>
2021-11-09 21:39:19 -0800

    V6 changing, lower the threshold of flushing score

--- utilities/DOTA/DOTA_tuner.cc
+++ utilities/DOTA/DOTA_tuner.cc
@@ -57,6 +57,7 @@ BatchSizeStallLevels DOTA_Tuner::LocateBatchStates(SystemScores &score) {
       if (score.active_size_ratio > 0.7 || score.immutable_number > 1) {
         return kTinyMemtable;
       }
+
     }
     if (score.flush_numbers < max_scores.flush_numbers * 0.3) {
       return kOverFrequent;
